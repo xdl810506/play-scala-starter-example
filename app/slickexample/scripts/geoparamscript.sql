a88f4076fa9223ca4fea2b995639b7d784167cce
jiangliu <jiangliu@qunhemail.com>
2018-08-17 17:41:21 +0800

    update the table for geometry param model

--- app/slickexample/scripts/geoparamscript.sql
+++ app/slickexample/scripts/geoparamscript.sql
@@ -4,11 +4,23 @@ create database geoparammodel;
 use geoparammodel;
 
 -- create tables
-drop table if exists scope;
-create table scope (
-  _id int not null primary key auto_increment,
-  scope char(36) not null,
-  family varchar(1024)
+drop table if exists geomodel;
+create table geomodel (
+  modelid int not null primary key auto_increment,
+  modeldataid varchar(255) not null,
+  paramtemplateid int not null,
+  type varchar(255),
+  created timestamp not null default ‘yyyy-mm-dd hh:mm:ss’ current_timestamp,
+  lastmodified timestamp not null default ‘yyyy-mm-dd hh:mm:ss’ current_timestamp on update current_timestamp
+) engine=InnoDB default charset=utf8;
+
+drop table if exists scripttemplate;
+create table scripttemplate (
+  templateid int not null primary key auto_increment,
+  name varchar(1024),
+  templatedataid varchar(255) not null,
+  created timestamp not null default ‘yyyy-mm-dd hh:mm:ss’ current_timestamp,
+  lastmodified timestamp not null default ‘yyyy-mm-dd hh:mm:ss’ current_timestamp on update current_timestamp
 ) engine=InnoDB default charset=utf8;
 
 show tables;
